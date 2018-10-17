1afb85f2b4f4a4496414bb8cd04f661844a622df
Max <maxkajiwara@gmail.com>
2018-10-16 23:17:41 -0700

    Fix action.payload, remove comment

--- lamboard/src/reducers/exampleReducer.js
+++ lamboard/src/reducers/exampleReducer.js
@@ -1,14 +1,14 @@
 import { EXAMPLE_ACTION } from '../actions';
 
 const initialState = {
-	exampleKey: 'exampleProp' // convered exampleProp into a string to stop error where exampleProp was not defined 
+	exampleKey: 'exampleProp'
 };
 
 const exampleReducer = (state = initialState, action) => {
 	switch (action.type) {
 		// example action
 		case EXAMPLE_ACTION:
-			return { ...state, exampleKey: 'payload' }; // commented payload out as we never defined the payload. This was preventing the app from running
+			return { ...state, exampleKey: action.payload };
 
 		default:
 			return state;
