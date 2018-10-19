f8be83d33e7dd43c75d6c0e14213a83e924aba80
Lauren <lndubose13@gmail.com>
2018-10-18 19:20:38 -0500

    Add ThreadPage (#5)
    Thread page is added on a different branch. Don't have a lot fo info to test
    out the branch adding.

--- lamboard/src/components/App.js
+++ lamboard/src/components/App.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import styled from 'styled-components';
 
+import ThreadPage from './ThreadPage';
 // Components
-import ExampleComponent from './ExampleComponent';
+import ExampleComponent from './exampleComponent';
 // import Navbar from './Navbar';
 // import HomePage from './HomePage';
 // import BoardPage from './BoardPage';
@@ -14,28 +15,28 @@ import ExampleComponent from './ExampleComponent';
 // import LoginPage from './LoginPage';
 
 const AppContainer = styled.div`
-	display: flex;
-	max-width: 1280px;
-	height: 100vh;
-	background: #c7e8f1;
-	margin: 0 auto;
+  display: flex;
+  max-width: 1280px;
+  height: 100vh;
+  background: #c7e8f1;
+  margin: 0 auto;
 `;
 
 class App extends Component {
-	render() {
-		return (
-			<AppContainer>
-				{/* <Navbar /> */}
-				<Route exact path="/" component={ExampleComponent} />
-				{/* <Route exact path="/" component={HomePage} /> */}
-				{/* <Route exact path="/:boardName" component={BoardPage} /> */}
-				{/* <Route path="/:boardName/:threadId" component={ThreadPage} /> */}
-				{/* <Route path="/users/:id" component={ProfilePage} /> */}
-				{/* <Route exact path="/register" component={SignupPage} /> */}
-				{/* <Route exact path="/login" component={LoginPage} /> */}
-			</AppContainer>
-		);
-	}
+  render() {
+    return (
+      <AppContainer>
+        {/* <Navbar /> */}
+        <Route exact path="/" component={ExampleComponent} />
+        {/* <Route exact path="/" component={HomePage} /> */}
+        {/* <Route exact path="/:boardName" component={BoardPage} /> */}
+        <Route path="/:boardName/:threadId" component={ThreadPage} />
+        {/* <Route path="/users/:id" component={ProfilePage} /> */}
+        {/* <Route exact path="/register" component={SignupPage} /> */}
+        {/* <Route exact path="/login" component={LoginPage} /> */}
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
---
+++ lamboard/src/components/ThreadPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ThreadPage() {
+  return <div>hi</div>;
+}
+
+export default ThreadPage;
