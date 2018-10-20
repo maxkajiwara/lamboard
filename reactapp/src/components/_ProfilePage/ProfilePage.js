a2d77da53691eef3d0501d953d2b4b44f5bc297d
michaelagard <mdagard@gmail.com>
2018-10-20 13:49:54 -0400

    add test state dummy data | styled-components | general styling shows the first style using a pastel color pallete.

--- reactapp/src/components/_ProfilePage/ProfilePage.js
+++ reactapp/src/components/_ProfilePage/ProfilePage.js
@@ -12,50 +12,16 @@ The start of the ProfilePage component
 import React, { Component } from 'react'
 import styled from 'styled-components';
 
-const testUser = {
-  name: 'Aklutch90001',
-  location: 'Los Angeles, California',
-  bio: 'Angel Investor, Tech Enthusiast, Acting'
-}
-
-const ProfilePageWrapper = styled.div`
-  display: flex;
-  background: green;
-  width: 100%;
-`;
-
-const ProfileInfo = styled.div`
-  display: flex;
-  height: 400px;
-`;
-
-const ProfileImg = styled.img`
-  width: 300px;
-  height: 400px;
-  background: blue;
-`;
-
-const ProfileBio = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  margin: 0 50px;
-`;
-
-const ProfileStats = styled.h4`
-  font-size: 40px;
-`;
-const ProfileStat = styled.span`
-  font-weight: bold;
-`;
-const ActivityButtons = styled.div`
-  
-`;
+// const MockAxiosPullFrom
 
 export default class ProfilePage extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { // test user
+      name: 'Aklutch90001',
+      location: 'Los Angeles, California',
+      bio: 'Angel Investor, Tech Enthusiast, Acting'
+    }
   }
 
   render() {
@@ -64,9 +30,9 @@ export default class ProfilePage extends Component {
         <ProfileInfo>
           <ProfileImg />
           <ProfileBio>
-            <ProfileStats><ProfileStat>Username</ProfileStat>: {testUser.name}</ProfileStats>
-            <ProfileStats><ProfileStat>Location</ProfileStat>: {testUser.location}</ProfileStats>
-            <ProfileStats><ProfileStat>Activities</ProfileStat>: {testUser.bio}</ProfileStats>
+            <ProfileStats><ProfileStat>Username</ProfileStat>: {this.state.name}</ProfileStats>
+            <ProfileStats><ProfileStat>Location</ProfileStat>: {this.state.location}</ProfileStats>
+            <ProfileStats><ProfileStat>Activities</ProfileStat>: {this.state.bio}</ProfileStats>
           </ProfileBio>
         </ProfileInfo>
         <ActivityButtons></ActivityButtons>
@@ -74,3 +40,49 @@ export default class ProfilePage extends Component {
     )
   }
 }
+
+// styled components
+const ProfilePageWrapper = styled.div`
+  display: flex;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 100px;
+  background: rgb(233,168,195); /* Old browsers */
+  background: -moz-linear-gradient(top, rgba(233,168,195,1) 0%, rgba(200,142,193,1) 100%); /* FF3.6-15 */
+  background: -webkit-linear-gradient(top, rgba(233,168,195,1) 0%,rgba(200,142,193,1) 100%); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(to bottom, rgba(233,168,195,1) 0%,rgba(200,142,193,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e9a8c3', endColorstr='#c88ec1',GradientType=0 ); /* IE6-9 */
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  height: 400px;
+  margin: 20px;
+`;
+
+const ProfileImg = styled.img`
+  width: 300px;
+  height: 400px;
+  background: #E9A8C3;
+  border: 3px solid #9397D1;
+  border-radius: 12px;
+`;
+
+const ProfileBio = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin: 0 50px;
+  width: 50%;
+`;
+
+const ProfileStats = styled.h4`
+  font-size: 40px;
+  color: #72496D;
+`;
+const ProfileStat = styled.span`
+  font-weight: bold;
+`;
+const ActivityButtons = styled.div`
+  
+`;
