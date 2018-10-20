0173492f9b101fcd8a494acb76460462c4b78f89
michaelagard <mdagard@gmail.com>
2018-10-19 21:41:09 -0400

    export ProfilePage as default

--- reactapp/src/components/index.js
+++ reactapp/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './__App/App';
 export { default as SignupPage } from './_SignupPage/SignupPage';
 export { default as ThreadPage } from './_ThreadPage/ThreadPage';
+export { default as ProfilePage } from './_ProfilePage/ProfilePage';
