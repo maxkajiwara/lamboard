6fefbeaa6c07c565df983d70fdf303cbae12a62c
Eva <36014601+eeevvaaa@users.noreply.github.com>
2018-10-22 21:04:33 -0500

    Feature/error handler (#13)
    * Delete CONTRIBUTING.md
    
    * Delete CONTRIBUTING.md (#9)
    
    * Delete CONTRIBUTING.md (#10)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Created error handler.

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,38 @@
+const errorHandler = (err, req, res, next) => {
+  const errors = {
+    h400: {
+      "httpStatus": 400,
+      "title": "Required field",
+      "description": "Missing input for required field",
+      "recoveryInstructions":"Please provide required information in your request and try again."
+    },
+    h401: {
+      "httpStatus": 401,
+      "title": "Unauthorized.",
+      "description": "Credentials provided were invalid.",
+      "recoveryInstructions": "Please enter valid credentials and try again."
+    },
+    h403: {
+      "httpStatus": 403,
+      "title": "Forbidden",
+      "description": "The user does not have the permission to access the request.",
+      "recoveryInstructions": "Please provide the correct ownership or permission."
+    },
+    h404: {
+      "httpStatus": 404,
+      "title": "Object not found",
+      "description": "The server was unable to find the specified object.",
+      "recoveryInstructions": "Please ensure to specified an existing object and try again."
+    },
+    h500: {
+      "httpStatus": 500,
+      "title": "Database error.",
+      "description": "The server is having trouble communicating with the database",
+      "recoveryInstructions": "Please contact server administration."
+    }
+  };
+  const error = errors[err];
+  res.status(error.httpStatus).json(error);
+}
+
+module.exports = errorHandler;
